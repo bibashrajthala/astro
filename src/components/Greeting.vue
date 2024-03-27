aafce0ab6aa0c11f464ebdc9bb00d391320afcfa
bibash.rajthala <bibash.rajthala@omnicommediagroup.com>
2024-03-27 13:25:14 +0545

    astro islands with vue js

---
+++ src/components/Greeting.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>{{ greeting }}! Thank you for visiting!</h3>
+    <button @click="greeting = randomMessage()">New Greeting</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "Greeting",
+  props: ["messages"],
+  setup(props) {
+    const greeting = ref(props?.messages[0]);
+    console.log(props.messages);
+
+    function randomMessage() {
+      return props.messages[Math.floor(Math.random() * props.messages.length)];
+    }
+
+    return {
+      greeting,
+      randomMessage,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
